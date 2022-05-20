8168a0852cdda8265c61c708054584b0516cc4d5
James Lave <jlave@integralads.com>
2022-05-19 23:31:50 -0500

    Try `waitForRequest` custom command

--- tests/jload-viewability.test.js
+++ tests/jload-viewability.test.js
@@ -4,10 +4,13 @@ describe('Jload', () => {
     // listen befor opening test page
     await browser.url('http://localhost:3000/jload-ad/direct');
 
+    
     // Custom Pause command from nightwatch-network-requests
     await browser.customPause(2000)
 
-    await browser.expect.element('body').to.be.present;
+    await browser.waitForRequest(/pingTime:1/);
+    
+    // await browser.expect.element('body').to.be.present;
       
     await browser.end();
   });
