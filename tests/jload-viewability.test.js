cdb7e09386a9304cc0f00ed2c530ebe496fe0b33
David Christian <dchristian@integralads.com>
2022-05-20 18:06:44 -0700

    Saving work

--- tests/jload-viewability.test.js
+++ tests/jload-viewability.test.js
@@ -2,10 +2,13 @@ describe('Jload', () => {
   it('should be in view', async (browser) =>{
 
     // listen befor opening test page
+    
     await browser.url('http://localhost:3000/jload-ad/direct');
+    
+    const requestRegExp = RegExp(/\/jload\?/);
+    const jloadRequest = await browser.waitForRequest(requestRegExp);
 
-    // Custom Pause command from nightwatch-network-requests
-    await browser.customPause(2000)
+    console.log('[Nightwatch-Sample-Project]::jloadRequest', jloadRequest);
 
     await browser.expect.element('body').to.be.present;
       
