ae232856569c31cad9fdfdebf640e1ac5590d6e2
David Christian <dchristian@integralads.com>
2022-05-23 07:57:54 -0700

    Saving progress - added waitForRequest

--- tests/jload-viewability.test.js
+++ tests/jload-viewability.test.js
@@ -1,16 +1,34 @@
+class NetworkRequest {
+	allRequests = [];
+
+	addRequest = (request) => {
+		this.allRequests.push(request);
+	};
+}
+
+const networkRequest = new NetworkRequest();
+
 describe('Jload', () => {
   it('should be in view', async (browser) =>{
 
-    // listen befor opening test page
-    
+    // Maybe try CDP Events next?
     await browser.url('http://localhost:3000/jload-ad/direct');
     
-    const requestRegExp = RegExp(/\/jload\?/);
-    const jloadRequest = await browser.waitForRequest(requestRegExp);
-
-    console.log('[Nightwatch-Sample-Project]::jloadRequest', jloadRequest);
-
     await browser.expect.element('body').to.be.present;
+
+    const jloadTagRequestRegExp = RegExp(/\/jload\?/);
+    const jloadRequest = await browser.waitForRequest(jloadTagRequestRegExp, networkRequest);
+    
+    console.log('Jload Tag Loaded');
+    
+    const impressionRequestRegExp = RegExp(/\/rfw\//);
+    await browser.waitForRequest(impressionRequestRegExp, networkRequest);
+    console.log('Recieved Impression Event');
+    
+    const inViewRequestRegExp = RegExp(/pingTime:1/);
+    await browser.waitForRequest(inViewRequestRegExp, networkRequest);
+    console.log('Recieved Viewability Ping');
+
       
     await browser.end();
   });
